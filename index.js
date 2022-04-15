0ba2fa28623132fc78b86f685470022ec577939f
Dumilde Matos <dumildejosematos@gmail.com>
2022-04-15 19:16:24 +0100

    connect to db and create Person model

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 
 // initial config
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
 
-// read Json using midleware
+const person = require('./models/Person');
+
+// read Json / midleware 
+// define request/response type
 app.use(
     express.urlencoded({
         extended: true,
@@ -12,9 +16,22 @@ app.use(
 
 app.use(express.json());
 
+app.post('/person', (req, res) => {
+
+});
+
 app.get('/', (req, res) => {
     res.json({message: 'Oi express'})
 })
 
 
-app.listen(3000)
+const DB_USER = 'root';
+const DB_PASS = 'DMyx7zZNl2fQpjNz'
+
+mongoose.connect(`mongodb+srv://${DB_USER}:${DB_PASS}@cluster0.q8sep.mongodb.net/bancoapinode1?retryWrites=true&w=majority`)
+.then(() => {
+    console.log('mongodb connected!');
+    app.listen(3000)
+})
+.catch(error => console.log(error))
+
